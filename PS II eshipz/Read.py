64f5777f36476dd8c4b55f34a13e3218e91b7c0b
Parth Kharade <kharade@pop-os.localdomain>
2020-08-04 15:12:29 +0530

    Converted MongoDB to Pandas DataFrame

--- PS II eshipz/Read.py
+++ PS II eshipz/Read.py
@@ -2,10 +2,9 @@ import pymongo
 import pandas as pd 
 
 myclient = pymongo.MongoClient("mongodb+srv://bitsps1:WJ70pOMfq8JOBQ42@fm-ps1-vquj0.gcp.mongodb.net/eshipz?retryWrites=true&w=majority")
-mydb=myclient["eshipz"]
 
-#db = pd.DataFrame(list(mydb))
-print(type(mydb))
-print(type(mydb))
-#for order in mydb.orders.find({}):
-#    print(order)
+data = myclient.eshipz.orders.find().limit(5)
+db = pd.DataFrame(data)
+cols= db.columns
+print(cols)
+#db.to_csv('Orders.csv')
