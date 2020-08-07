070ffdda106d131b9f154bdd7c1a33bff91b8ae8
Parth Kharade <parthkharade99@gmail.com>
2020-08-07 18:40:32 +0530

    City and State name extraction complete

--- PS II eshipz/analy.py
+++ PS II eshipz/analy.py
@@ -1,14 +1,28 @@
 import pandas as pd
-db = pd.read_csv('Orders.csv')
+from ast import literal_eval
+db  =   pd.read_csv('Orders.csv')
 
-db = db['order_details']
+db  =   db['order_details']
 
-IN = pd.DataFrame(columns = ['State','City'])
+PIN =   pd.read_csv('PIN.csv')
 
+i   = 0
+
+PIN.pincode = PIN.pincode.astype(str) #typecast the pincode values as string
+
+IN = pd.DataFrame(columns = ['State','City'])   #creating a dataframe to store the states and the cities
 IN_row = 0
 
-for a in db:
-    if(a['receiver_address']['country'] == 'IN'):
-        IN.loc[IN_row] = [a['receiver_address']['state'], a['receiver_address']['city']]
-        IN_row = IN_row + 1
+x   = pd.DataFrame()
 
+for a in db:
+    a  = literal_eval(a)
+    if(a['receiver_address']['country'] == 'IN'):
+        x = PIN.loc[PIN['pincode'] == a['receiver_address']['postal_code']]
+        x = x.loc[~(x['Taluk'] !=x['Taluk'])]
+        if(len(x.index)>0):
+            x = x.iloc[0]
+            IN.loc[IN_row] = x['statename'],x['Taluk']
+            IN_row = IN_row + 1
+IN.to_csv('Filtered.csv')
+print(IN)
